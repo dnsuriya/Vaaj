35db86c169bbd3cab82faa4088934f453542446e
Damith Nishshanka <dnsuriya@gmail.com>
2018-07-10 15:40:15 +1200

    First Commit

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+$to = "dnsuriya@gmail.com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+$headers .= 'From: <dnsuriya@gmail.com>' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+?>
